c22f1364f0ea7c501bb13700b716eb5ef92d1475
WestBP <55105272+WestBP@users.noreply.github.com>
2019-10-03 19:18:05 -0400

    Create Jet.h

---
+++ Jet.h
@@ -0,0 +1,29 @@
+
+// Created by Benjamin West on 10/3/19.
+
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+    int numberOfEngines;
+
+public:
+    Jet();
+
+    explicit Jet(string brand, string model, string fuelType,
+                 int numberOfEngines);
+
+    virtual ~Jet();
+    string getEngineSize();
+    void setNumberOfEngines(int numEngines);
+    virtual double mileageEstimate(double time);
+    virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_JET_H
